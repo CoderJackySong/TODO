61cf9f20a33baf2ceebfecf3c51180ad4b8fca90
JackySong <songyakui@zgyjyx.com>
2019-03-13 14:37:24 +0800

    使用UserDefaults方法将数据保存到本地。

--- TODO/TodoListViewController.swift
+++ TODO/TodoListViewController.swift
@@ -10,10 +10,16 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     var itemArray = ["购买水杯","吃药","修改密码"]
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+            itemArray = items
+        }
+        
+        
     }
     
     //MARK: - Table  View DataSource methods
@@ -28,7 +34,7 @@ class TodoListViewController: UITableViewController {
     }
     //MARK:- Table View Delegate methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(indexPath.row)
+        
         tableView.deselectRow(at: indexPath, animated: true)
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
@@ -40,7 +46,6 @@ class TodoListViewController: UITableViewController {
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
         var textField = UITextField()
-        
         let alert = UIAlertController(title: "添加一个新的ToDo项目", message: "", preferredStyle: .alert)
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "创建一个新项目..."
@@ -49,6 +54,7 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "添加项目", style: .cancel) { (ation) in
             print(textField.text!)
             self.itemArray.append(textField.text!)
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             self.tableView.reloadData()
         }
         alert.addAction(action)
